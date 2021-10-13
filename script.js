6a372cd3272b4f6c1d8512893f3b50a22b97fbd8
Robin Joshua Del Mundo <robin.delmundo@slidegenius.com>
2021-10-13 15:52:02 +0800

    Fixed Portfolio and Social Media Links

--- script.js
+++ script.js
@@ -10,16 +10,20 @@ function startup(){
 document.addEventListener("DOMContentLoaded", startup);
 
 function add_event_listeners(){
-	const about = document.querySelector('.about');
+	const about = document.querySelector('.theme-toggle');
 	about.addEventListener('click', toggle_dark_mode);
 }
 
 function toggle_dark_mode(){
 	const doc = document.body.parentElement;
+	const button = document.querySelector('#theme-toggle');
+	const para = document.querySelector('.about p');
 
 	if (doc.classList.contains('dark-mode') ){
 		doc.classList.remove('dark-mode');
+		para.innerHTML = 'Night Mode: Off';
 	} else{
 		doc.classList.add('dark-mode');
+		para.innerHTML = 'Night Mode: On';
 	}
 }
